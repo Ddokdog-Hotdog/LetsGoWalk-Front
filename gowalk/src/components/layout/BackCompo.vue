9d78bb8fc6410a46850ba8fa943ac01fecde4e28
leeeejsp <141730566+leeeejsp@users.noreply.github.com>
2024-09-05 10:04:51 +0900

    Add BackButton Component

---
+++ gowalk/src/components/layout/BackCompo.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="back">
+        <button class="back-button" @click="goToBack()">
+            <img :src="require('@/assets/icon/backButton-icon.png')" alt="Button 1 Icon" />
+        </button>
+        <pageTitleCompo />
+    </div>
+</template>
+
+<script>
+import pageTitleCompo from "@/components/layout/PageTitleCompo.vue";
+
+export default {
+    name: "backButton",
+    components: {
+        pageTitleCompo,
+    },
+    methods: {
+        goToBack() {
+            this.$router.go(-1);
+        },
+    },
+};
+</script>
+
+<style scoped>
+.back {
+    display: flex;
+    align-items: center;
+}
+
+.back-button {
+    margin-right: 10x;
+}
+
+/* .page-title {
+    flex: 1; 
+    text-align: center; 
+    margin-bottom: 5px;
+} */
+</style>
---
+++ gowalk/src/components/layout/PageTitleCompo.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="page-title">
+        <small>{{ title }}</small>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "PageTitle",
+    computed: {
+        title() {
+            const path = this.$route.path;
+            console.log(path);
+            if (path.includes("/shop/item")) {
+                return "상품 상세 페이지";
+            } else if (path === "/shop") {
+                return "쇼핑몰";
+            } else if (path === "/post") {
+                return "커뮤니티";
+            }
+            return "페이지";
+        },
+    },
+};
+</script>
+
+<style scoped>
+.page-title {
+    flex: 1;
+    text-align: center;
+    margin-bottom: 5px;
+}
+</style>
